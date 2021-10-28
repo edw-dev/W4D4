bc892ad84bcd28b6cb7fe72134fb367e267f74e4
stellalc7 <stellalc7@gmail.com>
2021-10-28 13:33:09 -0400

    spec wip

--- lib/array.rb
+++ lib/array.rb
@@ -1,9 +1,20 @@
 class Array
 
     def my_uniq
-        self.uniq
+        # self.uniq
+        arr=[]
+        self.each do |ele|
+            arr << ele if !arr.include?(ele)
+        end
+        arr
     end
     # [1, 2, 1, 3, 3].my_uniq # => [1, 2, 3]
 
+    def two_sum
+        arr=[]
+        self.each do |ele|
+        end
+        arr
+    end
 
 end
--- spec/tdd_projects_spec.rb
+++ spec/tdd_projects_spec.rb
@@ -5,5 +5,10 @@ describe "Array#my_uniq" do
     it "returns unique elements in order they appear" do
         expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
     end
+end
 
+describe "Array#two_sum" do
+    it "finds all pairs of positions that sum to zero" do
+        expect(.two_sum).to eq(0)
+    end
 end
